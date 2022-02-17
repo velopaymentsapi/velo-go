b5e30bab4c79bffb86e8d5b95f0c2f35974516e3
bhalle <britton@lklabs.com>
2022-02-17 18:46:38 +0000

    bump version to v2.29.128.beta1

--- configuration.go
+++ configuration.go
@@ -101,7 +101,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/2.26.124/go",
+		UserAgent:        "OpenAPI-Generator/2.29.128/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
